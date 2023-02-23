759427a6d021f6009f13f4eb0345208a76e4a3b2
Bruno Arouca <brunoarouca.dev@gmail.com>
2023-02-23 00:15:11 -0300

    Template projeto simples v1.0.0

---
+++ js/all.js
@@ -0,0 +1,1 @@
+"use strict";console.log("mais uma vez mudei o button");var number=32;console.log(number),console.log("modal");
---
+++ js/scripts/buttons.js
@@ -0,0 +1,5 @@
+{
+    console.log('mais uma vez mudei o button');
+    const number = 32;
+    console.log(number)
+}
